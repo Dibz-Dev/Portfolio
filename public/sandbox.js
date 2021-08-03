220cae241edf4cc9ddfcbd95dca446d7f5ae0667
devdibs <ben.dibley@hotmail.com>
2021-08-03 17:37:45 +0200

    changed index bio

--- public/sandbox.js
+++ public/sandbox.js
@@ -15,7 +15,8 @@ const feedback = document.querySelector('#feedback-container');
 
 
 
-const api_Url = "https://dibspersonal.herokuapp.com/getFeedback";
+// const api_Url = "https://dibspersonal.herokuapp.com/getFeedback";
+const api_Url = "http://localhost:3000/getFeedback";
 
 
 
